4b77c30760563b31d7693a932c1b8a2ee94a6a7c
luka8094 <luka8094@stud.kea.dk>
2023-04-17 10:05:15 +0200

    test: tested backend endpoint connection.

--- backend/server.js
+++ backend/server.js
@@ -3,4 +3,8 @@ import "dotenv/config"
 const app = express()
 const {PORT} = process.env
 
+app.get("/", (req, res) => {
+    res.send("Endpoint connection test status: OK")
+})
+
 app.listen(PORT, () => console.log("Server running on port: %s", PORT))
